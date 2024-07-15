3b86fceab101e53aff994436669ba95012c89381
saif80ali <mdsaif80ali@gmail.com>
2024-07-16 02:47:03 +0530

    Get user details

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -3,10 +3,12 @@ const userRouter = express.Router();
 const validateToken = require("../middleware/validateToken.js");
 
 const UserCompanyDetails = require("../model/user_information/userCompanyDetailsDB.js");
+const UserDB = require("../model/user_information/UserDB.js");
 
 userRouter.post("/get-details", validateToken,async (req, res) => {
     try {
-        res.status(200).send(req.userid);
+        data = await UserDB.findById(req.userid);
+        res.status(200).send(data);
     } catch (e) {
         res.status(400).send({status:"Failure", message:"Incorrect password"})
     }
